966284495dfa6e9a0820cd7d8ecb8f888b69b007
Thomas O Fredericks <thomas@fredericks.info>
2026-01-09 11:45:27 -0500

    Updated example

--- examples/osc_test/osc_test.ino
+++ examples/osc_test/osc_test.ino
@@ -2,22 +2,16 @@
 #include <Arduino.h>
 #include <MicroSetting.h>
 
-MicroSettingInt amp_env_attack("/attack", 0, 128, 2);
-MicroSettingInt amp_env_decay("/decay", 1, 2000, 400);
-MICRO_SETTING_GROUP(amp_env, "/env", &amp_env_attack, &amp_env_decay);
+MicroSettingGroup settings;
+MicroSettingInt amp_attack(settings, "/atk", 0, 128, 2);
+MicroSettingInt amp_decay(settings, "/dcy", 1, 2000, 400);
 
-MICRO_SETTING_GROUP(amp, "/amp", &amp_env);
+MicroSettingInt filter_attack(settings, "/vcf/atk", 0, 128, 4);
+MicroSettingInt filter_decay(settings, "/vcf/dcy", 1, 2000, 256);
+MicroSettingInt filter_mod(settings, "/vcf/mod", 0, 256, 32);
 
-MicroSettingInt filter_env_attack("/attack", 0, 128, 4);
-MicroSettingInt filter_env_decay("/decay", 1, 2000, 256);
-MicroSettingInt filter_env_amount("/amount", 0, 256, 32);
-MICRO_SETTING_GROUP(filterEnv, "/env", &filter_env_amount, &filter_env_attack, &filter_env_decay);
-
-MicroSettingInt filter_cutoff("/cutoff", 0, 128, 64);
-MicroSettingInt filter_resonance("/resonance", 0, 256, 128);
-MICRO_SETTING_GROUP(filter, "/filter", &filter_cutoff, &filter_resonance, &filterEnv);
-
-MICRO_SETTING_GROUP(settings, "/", &amp, &filter);
+MicroSettingInt filter_cutoff(settings, "/vcf/cut", 0, 128, 64);
+MicroSettingInt filter_resonance(settings, "/vcf/res", 0, 256, 128);
 
 #include <MicroOscSlip.h>
 // The number 128 between the < > below  is the maximum number of bytes reserved for incomming messages.
@@ -27,82 +21,61 @@ MicroOscSlip<128> myOsc(&Serial);
 unsigned long chrono;
 
 // FUNCTION THAT WILL BE CALLED WHEN AN OSC MESSAGE IS RECEIVED:
-void myOscMessageParser( MicroOscMessage& receivedOscMessage) {
-   // DO MESSAGE ADDRESS CHECKING AND ARGUMENT GETTING HERE
-   MicroSetting * match = matchPath( &settings, receivedOscMessage.getAddress(), '/' );
-   if ( match ) {
-    match->setInt(receivedOscMessage.nextAsInt());
-   }
-}
+void myOscMessageParser(MicroOsc & source, MicroOscMessage & receivedOscMessage)
+{
+    // DO MESSAGE ADDRESS CHECKING AND ARGUMENT GETTING HERE
 
-void setup() {
-  Serial.begin(115200);
+    size_t count = settings.getCount();
 
-  chrono = millis();
+    for (int32_t i = 0; i < count; ++i)
+    {
+        MicroSetting *setting = settings.get(i);
 
-}
-
-// Recursive function using a fixed-size buffer for full OSC path
-void sendOscRecursive(MicroSetting* setting, char* pathBuffer, size_t pathLen = 0) {
-    if (!setting) return;
-
-    const char* name = setting->getName();
-    size_t nameLen = strlen(name);
-
-    // Check buffer overflow
-    if (pathLen + nameLen >= 256) return; // adjust 256 to your max path length
-
-    // Append this setting's name to the path buffer
-    memcpy(pathBuffer + pathLen, name, nameLen);
-    size_t newLen = pathLen + nameLen;
-    pathBuffer[newLen] = '\0';
-
-    if (setting->getChildCount() == 0) {
-        // Leaf: send value over OSC
-        switch (setting->getType() ) {
-            case 'i':
-                myOsc.sendInt(pathBuffer, setting->getInt());
-                break;
-            case 'f':
-                myOsc.sendFloat(pathBuffer, setting->getFloat());
-                break;
-            case 'e':
-                myOsc.sendInt(pathBuffer, setting->getInt()); // enums as int
-                break;
-        }
-    } else {
-        // Group: recurse into children
-        for (int i = 0; i < setting->getChildCount(); i++) {
-            // Add '/' separator if needed (avoid double slashes)
-            size_t sepLen = 0;
-            if (setting->getChild(i)->getName()[0] != '/') {
-                if (newLen < 255) {
-                    pathBuffer[newLen] = '/';
-                    newLen++;
-                    pathBuffer[newLen] = '\0';
-                    sepLen = 1;
-                }
+        if (receivedOscMessage.checkOscAddress(setting->getName()))
+        {
+            if (receivedOscMessage.getTypeTag() == 'i')
+            {
+                setting->setInt(receivedOscMessage.nextAsInt());
             }
-            sendOscRecursive(setting->getChild(i), pathBuffer, newLen);
-            newLen -= sepLen; // remove separator after recursion
+            else if (receivedOscMessage.getTypeTag() == 'f')
+            {
+                setting->setFloat(receivedOscMessage.nextAsFloat());
+            }
+            break;
         }
     }
 }
 
+void setup()
+{
+    Serial.begin(115200);
 
-void loop() {
-
-      myOsc.onOscMessageReceived( myOscMessageParser );
-
-      if ( millis() - chrono >= 1000 ) {
-        chrono = millis();
-        char oscPath[256];  // max path length
-        oscPath[0] = '\0';  // start empty
-
-        sendOscRecursive(&settings, oscPath);
-
-        //myOsc.sendInt( filter_env_attack.getName(), filter_env_attack.getInt() );
-      }
-
+    chrono = millis();
 }
 
+void loop()
+{
+
+    myOsc.onOscMessageReceived(myOscMessageParser);
+
+    if (millis() - chrono >= 1000)
+    {
+        chrono = millis();
+
+        size_t count = settings.getCount();
+
+        for (int32_t i = 0; i < count; ++i)
+        {
+            MicroSetting *setting = settings.get(i);
+            if (setting->getType() == 'i')
+            {
+                myOsc.sendInt(setting->getName(), setting->getInt());
+            }
+            else if (setting->getType() == 'f')
+            {
+                myOsc.sendFloat(setting->getName(), setting->getFloat());
+            }
+        }
+
+    }
+}
