c566268412b09bc956ce31c7a9a964edea990a1f
Thomas O Fredericks <thomas@fredericks.info>
2026-01-09 14:30:07 -0500

    Cleanup

--- src/MicroSetting.h
+++ src/MicroSetting.h
@@ -20,10 +20,9 @@ class MicroSetting; // forward declaration
 
 class MicroSettingGroup
 {
-  MicroTof::ResizableArray<MicroSetting *> children_{4};
+  MicroTof::GrowableArray<MicroSetting *> children_{4};
 
 public:
-  // Constructor: optionally pass parent
   MicroSettingGroup()
   {
   }
@@ -44,11 +43,11 @@ public:
   }
 
   // Access a child by index
-  MicroSetting * get(size_t index)
+  MicroSetting *get(size_t index)
   {
     if (children_.getCount() == 0)
       return nullptr;
-    index = MicroTof::clampExclusive(index, 0, children_.getCount());
+    index = MicroTof::clamp(index, 0, children_.getCount());
     return children_[index];
   }
 };
@@ -60,7 +59,7 @@ protected:
   const char type_;
 
 public:
-  MicroSetting(MicroSettingGroup & group, const char *name, char type)
+  MicroSetting(MicroSettingGroup &group, const char *name, char type)
       : name_(name), type_(type)
   {
     group.add(this);
@@ -68,23 +67,14 @@ public:
 
   const char *getName() const { return name_; }
   char getType() { return type_; };
-  virtual void rotate(int32_t amount)
-  {
-  }
-  virtual void setFloat(float f)
-  {
-  }
-  virtual void setInt(int32_t i)
-  {
-  }
-  virtual float getFloat()
-  {
-    return 0;
-  }
-  virtual int32_t getInt()
-  {
-    return 0;
-  }
+
+  virtual void setFloat(float f) = 0;
+  virtual void setInt(int32_t i) = 0;
+  virtual float getFloat() = 0;
+  virtual int32_t getInt() = 0;
+  virtual void mapFloat(float in, float inMin, float inMax) = 0;
+  virtual void mapInt(int32_t in, int32_t inMin, int32_t inMax) = 0;
+
   /*   virtual int32_t getChildCount()
     {
       return 0; // returns 0 except for groups
@@ -103,30 +93,39 @@ private:
   int32_t max_;
 
 public:
-  MicroSettingInt(MicroSettingGroup &group, const char *name, int32_t min, int32_t maxExclusive, int32_t initial)
-      : MicroSetting(group, name, 'i'), min_(min), max_(maxExclusive)
+  MicroSettingInt(MicroSettingGroup &group, const char *name, int32_t min, int32_t maxInclusive, int32_t initial)
+      : MicroSetting(group, name, 'i'), min_(min), max_(maxInclusive)
   {
     setInt(initial);
   }
 
-  void setInt(int32_t i)
+  void setInt(int32_t i) override
   {
-    value_ = MicroTof::clampExclusive(i, min_, max_);
-  }
-
-  void rotate(int32_t amount) override
-  {
-    setInt(value_ + amount);
+    value_ = MicroTof::clamp(i, min_, max_);
   }
 
   int32_t getInt() { return value_; }
 
   void setFloat(float f) override
   {
-    setInt((int32_t)floor(f));
+    setInt(floor(f));
   }
 
   float getFloat() override { return (float)value_; }
+
+    void mapFloat(float in, float inMin, float inMax) override
+  {
+    // Scale to integer range
+    float mapped = MicroTof::mapf(in, inMin, inMax, min_, max_ );
+
+    setFloat(mapped);
+  }
+
+  void mapInt(int32_t in, int32_t inMin, int32_t inMaxExclusive) override
+  {
+    setInt(map(in, inMin, inMaxExclusive, min_, max_));
+  }
+
 };
 
 class MicroSettingFloat : public MicroSetting
@@ -149,14 +148,9 @@ public:
   {
   }
 
-  void setFloat(float f)
+  void setFloat(float f) override
   {
-    value_ = MicroTof::clampInclusivef(f, min_, max_);
-  }
-
-  void rotate(int32_t amount) override
-  {
-    setFloat(value_ + step_ * amount);
+    value_ = MicroTof::clampf(f, min_, max_);
   }
 
   float getFloat() { return value_; }
@@ -167,6 +161,20 @@ public:
   }
 
   int32_t getInt() override { return floor(value_); }
+  
+    void mapFloat(float in, float inMin, float inMax) override
+  {
+    // Scale to integer range
+    float mapped = MicroTof::mapf(in, inMin, inMax, min_, max_ );
+
+    setFloat(mapped);
+  }
+
+  void mapInt(int32_t in, int32_t inMin, int32_t inMaxExclusive) override
+  {
+    setInt(map(in, inMin, inMaxExclusive, min_, max_));
+  }
+  
 };
 
 class MicroSettingEnum : public MicroSetting
@@ -182,12 +190,7 @@ public:
 
   void setInt(int32_t i) override
   {
-    value_ = MicroTof::wrapExclusive(i, 0, count_);
-  }
-
-  void rotate(int32_t amount) override
-  {
-    setInt(value_ + amount);
+    value_ = MicroTof::modulo(i, count_);
   }
 
   int32_t getInt() override { return value_; }
@@ -198,6 +201,19 @@ public:
   }
 
   float getFloat() override { return (float)value_; }
+
+    void mapFloat(float in, float inMin, float inMax) override
+  {
+    // Scale to integer range
+    float mapped = MicroTof::mapf(in, inMin, inMax, 0, count_ - 1 );
+
+    setFloat(mapped);
+  }
+
+  void mapInt(int32_t in, int32_t inMin, int32_t inMaxExclusive) override
+  {
+    setInt(map(in, inMin, inMaxExclusive, 0, count_ - 1));
+  }
 };
 
 /*
@@ -223,75 +239,4 @@ public:
 
 #endif
 };
-
-
-
-/*
-#ifdef ESP32
-public:
-  virtual void putInPreferences(Preferences *preferences)
-  {
-    switch (type_)
-    {
-    case 'e':
-    case 'i':
-      preferences->putInt(name_, value_.i);
-      break;
-    }
-  }
-
-  virtual void getFromPreferences(Preferences *preferences)
-  {
-
-    switch (type_)
-    {
-    case 'e':
-    case 'i':
-      setInt(preferences->getInt(name_, value_.i));
-      break;
-    }
-  }
-
-#endif
 */
-
-/*
-MicroSetting *matchPath(MicroSetting *root, const char *path, char separator)
-{
-    if (!root || !path) return nullptr;
-
-    MicroSetting *current = root;
-
-    while (*path && current)
-    {
-        // Find end of current segment
-        const char *segmentEnd = strchr(path, separator);
-        size_t segmentLen = segmentEnd ? (size_t)(segmentEnd - path) : strlen(path);
-
-        // Search children
-        bool found = false;
-        int count = current->getChildCount();
-
-        for (int i = 0; i < count; i++)
-        {
-            MicroSetting *child = current->getChild(i);
-            const char *name = child->getName();
-
-            // Match including the leading '/'
-            if (strncmp(name, path, segmentLen) == 0 && name[segmentLen] == '\0')
-            {
-                current = child;
-                found = true;
-                break;
-            }
-        }
-
-        if (!found) return nullptr;
-
-        // Move to next segment
-        path += segmentLen;
-        if (*path == separator) path++;
-    }
-
-    return current;
-} */
