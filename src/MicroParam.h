95fe826e2171c80069154bea4449bda8e7d2f271
Thomas O Fredericks <thomas@fredericks.info>
2026-01-12 12:48:44 -0500

    Update dependencies from MicroTof to MicroCommon and adjust related includes

--- src/MicroParam.h
+++ src/MicroParam.h
@@ -3,7 +3,7 @@
 #define __MICRO_PARAM_H__
 
 #include <Arduino.h>
-#include <MicroTof.h>
+#include <MicroCommon.h>
 
 class MicroParam
 {
@@ -36,7 +36,7 @@ private:
 public:
   void setInt(int32_t i) override
   {
-    value_ = MicroTof::clamp<int32_t>(i, min_, max_);
+    value_ = Micro::clamp<int32_t>(i, min_, max_);
   }
 
   int32_t getInt() const override { return value_; }
@@ -50,13 +50,13 @@ public:
 
   void mapFloat(float in, float inMin, float inMax) override
   {
-    float mapped = MicroTof::map<float>(in, inMin, inMax, min_, max_);
+    float mapped = Micro::map<float>(in, inMin, inMax, min_, max_);
     setFloat(mapped);
   }
 
   void mapInt(int32_t in, int32_t inMin, int32_t inMax) override
   {
-    setInt(MicroTof::map<int32_t>(in, inMin, inMax, min_, max_));
+    setInt(Micro::map<int32_t>(in, inMin, inMax, min_, max_));
   }
 
   MicroParamInt(int32_t initial, int32_t min, int32_t max)
@@ -77,7 +77,7 @@ private:
 public:
   void setFloat(float f) override
   {
-    value_ = MicroTof::clamp<float>(f, min_, max_);
+    value_ = Micro::clamp<float>(f, min_, max_);
   }
 
   float getFloat() const override { return value_; }
@@ -92,14 +92,14 @@ public:
   void mapFloat(float in, float inMin, float inMax) override
   {
     // Scale to integer range
-    float mapped = MicroTof::map<float>(in, inMin, inMax, min_, max_);
+    float mapped = Micro::map<float>(in, inMin, inMax, min_, max_);
 
     setFloat(mapped);
   }
 
   void mapInt(int32_t in, int32_t inMin, int32_t inMax) override
   {
-    setInt(MicroTof::map<int32_t>(in, inMin, inMax, min_, max_));
+    setInt(Micro::map<int32_t>(in, inMin, inMax, min_, max_));
   }
 
   MicroParamFloat( float initial, float min, float max)
@@ -128,7 +128,7 @@ public:
     // MicroParam int interface
     void setInt(int32_t i) override
   {
-    value_ = MicroTof::clamp<int32_t>(i, min_, max_);
+    value_ = Micro::clamp<int32_t>(i, min_, max_);
   }
 
   int32_t getInt() const override { return value_; }
@@ -142,13 +142,13 @@ public:
 
   void mapFloat(float in, float inMin, float inMax) override
   {
-    float mapped = MicroTof::map<float>(in, inMin, inMax, min_, max_);
+    float mapped = Micro::map<float>(in, inMin, inMax, min_, max_);
     setFloat(mapped);
   }
 
   void mapInt(int32_t in, int32_t inMin, int32_t inMax) override
   {
-    setInt(MicroTof::map<int32_t>(in, inMin, inMax, min_, max_));
+    setInt(Micro::map<int32_t>(in, inMin, inMax, min_, max_));
   }
 }; */
 
@@ -166,7 +166,7 @@ public:
 
   void setInt(int32_t i) override
   {
-    value_ = MicroTof::modulo(i, count_);
+    value_ = Micro::modulo(i, count_);
   }
 
   int32_t getInt() override { return value_; }
@@ -181,7 +181,7 @@ public:
   void mapFloat(float in, float inMin, float inMax) override
   {
     // Scale to integer range
-    float mapped = MicroTof::mapf(in, inMin, inMax, 0, count_ - 1);
+    float mapped = Micro::mapf(in, inMin, inMax, 0, count_ - 1);
 
     setFloat(mapped);
   }
@@ -193,8 +193,8 @@ public:
 };
 */
 
-typedef MicroTof::Bind<MicroParam *> MicroParamBind;
-typedef MicroTof::Binder<MicroParam *> MicroParamBinder;
+typedef Micro::Bind<MicroParam *> MicroParamBind;
+typedef Micro::Binder<MicroParam *> MicroParamBinder;
 
 /* class Bindings
 {
