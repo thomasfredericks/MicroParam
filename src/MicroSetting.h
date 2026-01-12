8f8f0c4af3b83158eb3acb1ca0a9fc96371430af
Thomas O Fredericks <thomas@fredericks.info>
2026-01-11 22:19:32 -0500

    Getting ready to rename to MicroParam

--- src/MicroSetting.h
+++ src/MicroSetting.h
@@ -1,91 +1,32 @@
 
-#include <Arduino.h>
-#include <MicroTof.h>
-
-#ifdef ESP32
-#include <Preferences.h>
-#endif
+#ifndef __MICRO_PARAM_H__
+#define __MICRO_PARAM_H__
 
 #include <Arduino.h>
 #include <MicroTof.h>
-/*
-#define MICRO_SETTING_ARRAY_COUNT(x) (sizeof(x) / sizeof((x)[0]))
 
-#define MICRO_SETTING_GROUP(var, ...)             \
-  MicroSetting *var##_children[] = {__VA_ARGS__}; \
-  MicroSettingGroup var(var##_children, MICRO_SETTING_ARRAY_COUNT(var##_children))
-*/
-
-class MicroSetting; // forward declaration
-
-class MicroSettingGroup
+class MicroParam
 {
-  MicroTof::GrowableArray<MicroSetting *> children_{4};
+
+private:
+  char type_;
 
 public:
-  MicroSettingGroup()
+  MicroParam(char type) : type_(type)
   {
   }
 
-  // Add a child setting (value or group)
-  void add(MicroSetting *child)
-  {
-    if (child)
-    {
-      children_.add(child);
-    }
-  }
+  virtual void setInt(int32_t v) = 0;
+  virtual int32_t getInt() const = 0;
 
-  // Number of children
-  size_t getCount() const
-  {
-    return children_.getCount();
-  }
+  virtual void setFloat(float v) = 0;
+  virtual float getFloat() const = 0;
 
-  // Access a child by index
-  MicroSetting *get(size_t index)
-  {
-    if (children_.getCount() == 0)
-      return nullptr;
-    index = MicroTof::clamp(index, 0, children_.getCount());
-    return children_[index];
-  }
-};
-
-class MicroSetting
-{
-protected:
-  const char *name_;
-  const char type_;
-
-public:
-  MicroSetting(MicroSettingGroup &group, const char *name, char type)
-      : name_(name), type_(type)
-  {
-    group.add(this);
-  }
-
-  const char *getName() const { return name_; }
-  char getType() { return type_; };
-
-  virtual void setFloat(float f) = 0;
-  virtual void setInt(int32_t i) = 0;
-  virtual float getFloat() = 0;
-  virtual int32_t getInt() = 0;
   virtual void mapFloat(float in, float inMin, float inMax) = 0;
   virtual void mapInt(int32_t in, int32_t inMin, int32_t inMax) = 0;
-
-  /*   virtual int32_t getChildCount()
-    {
-      return 0; // returns 0 except for groups
-    };
-    virtual MicroSetting *getChild(int32_t index)
-    {
-      return nullptr; // returns nullptr except for groups
-    } */
 };
 
-class MicroSettingInt : public MicroSetting
+class MicroParamInt : public MicroParam
 {
 private:
   int32_t value_;
@@ -93,79 +34,65 @@ private:
   int32_t max_;
 
 public:
-  MicroSettingInt(MicroSettingGroup &group, const char *name, int32_t min, int32_t maxInclusive, int32_t initial)
-      : MicroSetting(group, name, 'i'), min_(min), max_(maxInclusive)
-  {
-    setInt(initial);
-  }
-
   void setInt(int32_t i) override
   {
-    value_ = MicroTof::clamp(i, min_, max_);
+    value_ = MicroTof::clamp<int32_t>(i, min_, max_);
   }
 
-  int32_t getInt() { return value_; }
+  int32_t getInt() const override { return value_; }
 
   void setFloat(float f) override
   {
     setInt(floor(f));
   }
 
-  float getFloat() override { return (float)value_; }
+  float getFloat() const override { return (float)value_; }
 
-    void mapFloat(float in, float inMin, float inMax) override
+  void mapFloat(float in, float inMin, float inMax) override
   {
-    // Scale to integer range
-    float mapped = MicroTof::mapf(in, inMin, inMax, min_, max_ );
-
+    float mapped = MicroTof::map<float>(in, inMin, inMax, min_, max_);
     setFloat(mapped);
   }
 
-  void mapInt(int32_t in, int32_t inMin, int32_t inMaxExclusive) override
+  void mapInt(int32_t in, int32_t inMin, int32_t inMax) override
   {
-    setInt(map(in, inMin, inMaxExclusive, min_, max_));
+    setInt(MicroTof::map<int32_t>(in, inMin, inMax, min_, max_));
   }
 
+  MicroParamInt(int32_t initial, int32_t min, int32_t max)
+      : MicroParam('i')
+  {
+    setInt(initial);
+  }
 };
 
-class MicroSettingFloat : public MicroSetting
+
+class MicroParamFloat : public MicroParam
 {
 private:
   float value_;
   float min_;
   float max_;
-  float step_;
 
 public:
-  MicroSettingFloat(MicroSettingGroup &group, const char *name, float min, float maxInclusive, float initial, float step)
-      : MicroSetting(group, name, 'f'), min_(min), max_(maxInclusive), step_(step)
-  {
-    setFloat(initial);
-  }
-
-  MicroSettingFloat(MicroSettingGroup &group, const char *name, float min, float maxInclusive, float initial)
-      : MicroSettingFloat(group, name, min, maxInclusive, initial, 0.1f)
-  {
-  }
-
   void setFloat(float f) override
   {
-    value_ = MicroTof::clampf(f, min_, max_);
+    value_ = MicroTof::clamp<float>(f, min_, max_);
   }
 
-  float getFloat() { return value_; }
+  float getFloat() const override { return value_; }
 
   void setInt(int32_t i) override
   {
     setFloat((float)i);
   }
 
-  int32_t getInt() override { return floor(value_); }
-  
-    void mapFloat(float in, float inMin, float inMax) override
+  int32_t getInt() const override { return floor(value_); }
+
+  void mapFloat(float in, float inMin, float inMax) override
   {
     // Scale to integer range
-    float mapped = MicroTof::mapf(in, inMin, inMax, min_, max_ );
+    float mapped = MicroTof::map<float>(in, inMin, inMax, min_, max_);
 
     setFloat(mapped);
   }
@@ -174,10 +101,17 @@ public:
   {
     setInt(map(in, inMin, inMaxExclusive, min_, max_));
   }
-  
+
+  MicroParamFloat( float initial, float min, float max)
+      : MicroParam('f'), min_(min), max_(max)
+  {
+    setFloat(initial);
+  }
+
 };
 
-class MicroSettingEnum : public MicroSetting
+/*
+class MicroParamEnum : public MicroParam
 {
 private:
   int32_t value_;
@@ -185,8 +119,8 @@ private:
   int32_t count_;
 
 public:
-  MicroSettingEnum(MicroSettingGroup &group, const char *name, const char **labels, int32_t count)
-      : MicroSetting(group, name, 'e'), labels_(labels), count_(count), value_(0) {}
+  MicroParamEnum( const char **labels, int32_t count)
+      : MicroParam('e'), labels_(labels), count_(count), value_(0) {}
 
   void setInt(int32_t i) override
   {
@@ -202,10 +136,10 @@ public:
 
   float getFloat() override { return (float)value_; }
 
-    void mapFloat(float in, float inMin, float inMax) override
+  void mapFloat(float in, float inMin, float inMax) override
   {
     // Scale to integer range
-    float mapped = MicroTof::mapf(in, inMin, inMax, 0, count_ - 1 );
+    float mapped = MicroTof::mapf(in, inMin, inMax, 0, count_ - 1);
 
     setFloat(mapped);
   }
@@ -215,28 +149,5 @@ public:
     setInt(map(in, inMin, inMaxExclusive, 0, count_ - 1));
   }
 };
-
-/*
-#ifdef ESP32
-
-  void putEachInPreferences(Preferences *preferences)
-  {
-    for (int i = 0; i < count_; i++)
-    {
-      // LOG("Putting", i, settings_[i]->getName() );
-      settings_[i]->putInPreferences(preferences);
-    }
-  }
-
-  void getEachFromPreferences(Preferences *preferences)
-  {
-    for (int i = 0; i < count_; i++)
-    {
-      // LOG("Getting", i, settings_[i]->getName() );
-      settings_[i]->getFromPreferences(preferences);
-    }
-  }
-
-#endif
-};
 */
+#endif // __MICRO_PARAM_H__
