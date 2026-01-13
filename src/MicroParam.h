8ca30b69a52cc17206117f39c69c3b732e5e5739
Thomas O Fredericks <thomas@fredericks.info>
2026-01-13 16:36:18 -0500

    Started printing logic

--- src/MicroParam.h
+++ src/MicroParam.h
@@ -8,7 +8,7 @@
 class MicroParam
 {
 
-private:
+protected:
   char type_;
 
 public:
@@ -27,14 +27,18 @@ public:
 
   virtual void mapFloat(float in, float inMin, float inMax) = 0;
   virtual void mapInt(int32_t in, int32_t inMin, int32_t inMax) = 0;
+
+  virtual void printDefinition(Stream & stream, char seperator) = 0;
+  virtual void printValue(Stream & stream) = 0;
+  
 };
 
 class MicroParamInt : public MicroParam
 {
 private:
   int32_t value_;
-  int32_t min_;
-  int32_t max_;
+  const int32_t min_;
+  const int32_t max_;
 
 public:
   void setInt(int32_t i) override
@@ -62,6 +66,16 @@ public:
     setInt(Micro::map<int32_t>(in, inMin, inMax, min_, max_));
   }
 
+  void printValue(Stream & stream) override {
+      stream.print(value_);
+  }
+
+  void printDefinition(Stream & stream, char seperator ) override {
+    stream.print(min_);
+    stream.print(seperator);
+    stream.print(max_);
+  }
+
   MicroParamInt(int32_t initial, int32_t min, int32_t max)
       : MicroParam('i'), min_(min), max_(max)
   {
@@ -74,8 +88,8 @@ class MicroParamFloat : public MicroParam
 {
 private:
   float value_;
-  float min_;
-  float max_;
+  const float min_;
+  const float max_;
 
 public:
   void setFloat(float f) override
@@ -105,6 +119,17 @@ public:
     setInt(Micro::map<int32_t>(in, inMin, inMax, min_, max_));
   }
 
+ void printValue(Stream & stream) override {
+      stream.print(value_);
+  }
+
+  void printDefinition(Stream & stream, char seperator ) override {
+    stream.print(min_);
+    stream.print(seperator);
+    stream.print(max_);
+
+  }
+
   MicroParamFloat( float initial, float min, float max)
       : MicroParam('f'), min_(min), max_(max)
   {
@@ -113,47 +138,6 @@ public:
 
 };
 
-/* class MicroParamBindInt : public MicroParam
-{
-    int32_t & value_; // reference to the actual int
-    int32_t min_ = 0;
-    int32_t max_ = 0;
-
-public:
-    // constructor: type 'i' for int, key not stored in base
-    MicroParamBindInt(int32_t &value, int32_t min, int32_t max)
-        : MicroParam('i'), value_(value), min_(min), max_(max)
-    {
-        setInt(value); // clamp initial value
-    }
-
-    // ===============================
-    // MicroParam int interface
-    void setInt(int32_t i) override
-  {
-    value_ = Micro::clamp<int32_t>(i, min_, max_);
-  }
-
-  int32_t getInt() const override { return value_; }
-
-  void setFloat(float f) override
-  {
-    setInt(floor(f));
-  }
-
-  float getFloat() const override { return (float)value_; }
-
-  void mapFloat(float in, float inMin, float inMax) override
-  {
-    float mapped = Micro::map<float>(in, inMin, inMax, min_, max_);
-    setFloat(mapped);
-  }
-
-  void mapInt(int32_t in, int32_t inMin, int32_t inMax) override
-  {
-    setInt(Micro::map<int32_t>(in, inMin, inMax, min_, max_));
-  }
-}; */
 
 /*
 class MicroParamEnum : public MicroParam
@@ -202,39 +186,17 @@ typedef Micro::Bind<MicroParam *> MicroParamBind;
 // Example use MicroParamBinder binder(bindings, sizeof(bindings) / sizeof(MicroParamBind));
 typedef Micro::Binder<MicroParam *> MicroParamBinder;
 
-/* class Bindings
-{
-
-    size_t count_ = 0;
-    MicroParamBind * pairs_;
-
+/* class MicroParamBinder : public Micro::Binder<MicroParam*> {
 public:
-    KeyParamCollection(MicroParamBind * pairs, size_t count)
-        : pairs_(pairs), count_(count)
-    {
-
+    using Micro::Binder<MicroParam*>::Binder;
+    void printConfig(size_t index, Stream * stream) {
+            sttream->print(binder.getKey(i));
+            stream->pprint(' ');
+            getPointer(i)->printConfig(stream);
+            stream->println();
     }
-
-    // get pointer to the stored value by key
-    MicroParam * get(const char *key)
-    {
-        for (size_t i = 0; i < count_; ++i)
-        {
-            if (strcmp(pairs_[i].key_, key) == 0)
-                return pairs_[i].pointer_;
-        }
-        return nullptr;
-    }
-
-    // get pointer by index
-    MicroParam * get(size_t index)
-    {
-        if (index >= count_)
-            return nullptr;
-        return pairs_[index].pointer_;
-    }
-
-    size_t getCount() const { return count_; }
 }; */
+
+
 
 #endif // __MICRO_PARAM_H__
