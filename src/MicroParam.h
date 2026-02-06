949599aaa7143d602bb7dcc2699b6f67ca7a79f1
Thomas O Fredericks <thomas@fredericks.info>
2026-02-06 12:17:10 -0500

    Finished first new implementation

--- src/MicroParam.h
+++ src/MicroParam.h
@@ -5,44 +5,43 @@
 #include <Arduino.h>
 #include <MicroCommon.h>
 
-namespace MicroParam
+// ======================= Base class =======================
+class MicroParam
 {
-  enum Type
-  {
-    INT = 'i',
-    FLOAT = 'f',
-    ENUM = 'e',
-    STRING = 's',
-    BYTE = 'y',
-    BLOB = 'b'
-  };
-}
-
-// ======================= Base types =======================
-class MicroParam 
-{
+public:
+    enum Type
+    {
+        INT = 'i',
+        FLOAT = 'f',
+        ENUM = 'e',
+        STRING = 's',
+        BYTE = 'y',
+        BLOB = 'b'
+    };
 
 protected:
-  char type_;
+    const MicroParam::Type type_;
 
 public:
-  MicroParam(char type) : type_(type)
-  {
-  }
+    MicroParam(MicroParam::Type type) : type_(type)
+    {
+    }
 
-  char getType()
-  {
-    return type_;
-  }
-  virtual void setInt(int32_t v) = 0;
-  virtual int32_t getInt() const = 0;
+    MicroParam::Type getType()
+    {
+        return type_;
+    }
+    virtual void setInt(int32_t v) = 0;
+    virtual int32_t getInt() const = 0;
 
-  virtual void setFloat(float v) = 0;
-  virtual float getFloat() const = 0;
+    virtual void setFloat(float v) = 0;
+    virtual float getFloat() const = 0;
 
-  virtual void mapFloat(float in, float inMin, float inMax) = 0;
-  virtual void mapInt(int32_t in, int32_t inMin, int32_t inMax) = 0;
+    virtual void mapFloat(float in, float inMin, float inMax) = 0;
+    virtual void mapInt(int32_t in, int32_t inMin, int32_t inMax) = 0;
 };
+
+// ======================= Simple types =======================
 
 class MicroParamByte : public MicroParam
 {
@@ -53,10 +52,12 @@ private:
 
 public:
     MicroParamByte(uint8_t v, uint8_t min = 0, uint8_t max = 255)
-        : MicroParam('b'), value_(v), min_(min), max_(max)
+        : MicroParam(MicroParam::Type::BYTE), value_(v), min_(min), max_(max)
     {
-        if (value_ < min_) value_ = min_;
-        if (value_ > max_) value_ = max_;
+        if (value_ < min_)
+            value_ = min_;
+        if (value_ > max_)
+            value_ = max_;
     }
 
     // Assignment operators
@@ -74,40 +75,13 @@ public:
     // Implicit conversion
     operator uint8_t() const { return value_; }
 
-    // Prefix increment / decrement
-    MicroParamByte &operator++()
-    {
-        if (value_ < max_) value_++;
-        return *this;
-    }
-
-    MicroParamByte operator++(int)
-    {
-        MicroParamByte temp = *this;
-        ++(*this);
-        return temp;
-    }
-
-    MicroParamByte &operator--()
-    {
-        if (value_ > min_) value_--;
-        return *this;
-    }
-
-    MicroParamByte operator--(int)
-    {
-        MicroParamByte temp = *this;
-        --(*this);
-        return temp;
-    }
-
+   
     // Explicit API
-    void set(uint8_t v) { *this = v; }
-    uint8_t get() const { return value_; }
+   // void set(uint8_t v) { *this = v; }
+   // uint8_t get() const { return value_; }
 };
 
-
-class MicroParamInt  : public MicroParam
+class MicroParamInt : public MicroParam
 {
 private:
     int32_t value_;
@@ -116,10 +90,26 @@ private:
 
 public:
     // Assignment operators
-    MicroParamInt& operator=(int32_t v) { value_ = microClamp<int32_t>(v, min_, max_); return *this; }
-    MicroParamInt& operator=(int v)       { value_ = microClamp<int32_t>((int32_t)v, min_, max_); return *this; }
-    MicroParamInt& operator=(float v)     { value_ = microClamp<int32_t>((int32_t)floor(v), min_, max_); return *this; }
-    MicroParamInt& operator=(const MicroParamInt& other) { value_ = other.value_; return *this; }
+    MicroParamInt &operator=(int32_t v)
+    {
+        value_ = microClamp<int32_t>(v, min_, max_);
+        return *this;
+    }
+    MicroParamInt &operator=(int v)
+    {
+        value_ = microClamp<int32_t>((int32_t)v, min_, max_);
+        return *this;
+    }
+    MicroParamInt &operator=(float v)
+    {
+        value_ = microClamp<int32_t>((int32_t)floor(v), min_, max_);
+        return *this;
+    }
+    MicroParamInt &operator=(const MicroParamInt &other)
+    {
+        value_ = other.value_;
+        return *this;
+    }
 
     // Implicit conversion
     operator int32_t() const { return value_; }
@@ -143,13 +133,13 @@ public:
     }
 
     MicroParamInt(int32_t initial, int32_t min, int32_t max)
-        : MicroParam('i'), min_(min), max_(max)
+        : MicroParam(MicroParam::Type::INT), min_(min), max_(max)
     {
         value_ = microClamp<int32_t>(initial, min_, max_);
     }
 };
 
-class MicroParamFloat  : public MicroParam
+class MicroParamFloat : public MicroParam
 {
 private:
     float value_;
@@ -158,14 +148,30 @@ private:
 
 public:
     // Assignment operators
-    MicroParamFloat& operator=(float v)     { value_ = microClamp<float>(v, min_, max_); return *this; }
-    MicroParamFloat& operator=(int32_t v)   { value_ = microClamp<float>((float)v, min_, max_); return *this; }
-    MicroParamFloat& operator=(int v)       { value_ = microClamp<float>((float)v, min_, max_); return *this; }
-    MicroParamFloat& operator=(const MicroParamFloat& other) { value_ = other.value_; return *this; }
+    MicroParamFloat &operator=(float v)
+    {
+        value_ = microClamp<float>(v, min_, max_);
+        return *this;
+    }
+    MicroParamFloat &operator=(int32_t v)
+    {
+        value_ = microClamp<float>((float)v, min_, max_);
+        return *this;
+    }
+    MicroParamFloat &operator=(int v)
+    {
+        value_ = microClamp<float>((float)v, min_, max_);
+        return *this;
+    }
+    MicroParamFloat &operator=(const MicroParamFloat &other)
+    {
+        value_ = other.value_;
+        return *this;
+    }
 
     // Implicit conversion
     operator float() const { return value_; }
-   operator int32_t() const { return (int32_t)floor(value_); }
+    operator int32_t() const { return (int32_t)floor(value_); }
 
     // Virtual API
     void setFloat(float f) override { value_ = microClamp<float>(f, min_, max_); }
@@ -185,12 +191,15 @@ public:
     }
 
     MicroParamFloat(float initial, float min, float max)
-        : MicroParam('f'), min_(min), max_(max)
+        : MicroParam(MicroParam::Type::FLOAT), min_(min), max_(max)
     {
         value_ = microClamp<float>(initial, min_, max_);
     }
 };
-class MicroParamEnum  : public MicroParam
+
+// ---------------
+
+class MicroParamEnum : public MicroParam
 {
 private:
     int32_t value_;
@@ -199,14 +208,30 @@ private:
 
 public:
     // Assignment operators
-    MicroParamEnum& operator=(int32_t v) { value_ = microModulo(v, count_); return *this; }
-  MicroParamEnum& operator=(int v)     { value_ = microModulo((int32_t)v, count_); return *this; }
-  MicroParamEnum& operator=(float v)   { value_ = microModulo((int32_t)floor(v), count_); return *this; }
-   MicroParamEnum& operator=(const MicroParamEnum& other) { value_ = other.value_; return *this; }
+    MicroParamEnum &operator=(int32_t v)
+    {
+        value_ = microModulo(v, count_);
+        return *this;
+    }
+    MicroParamEnum &operator=(int v)
+    {
+        value_ = microModulo((int32_t)v, count_);
+        return *this;
+    }
+    MicroParamEnum &operator=(float v)
+    {
+        value_ = microModulo((int32_t)floor(v), count_);
+        return *this;
+    }
+    MicroParamEnum &operator=(const MicroParamEnum &other)
+    {
+        value_ = other.value_;
+        return *this;
+    }
 
     // Implicit conversion
     operator int32_t() const { return value_; }
-   operator float() const { return (float)value_; }
+    operator float() const { return (float)value_; }
 
     // Virtual API
     void setInt(int32_t i) override { value_ = microModulo(i, count_); }
@@ -217,22 +242,22 @@ public:
 
     void mapFloat(float in, float inMin, float inMax) override
     {
-        value_ = microModulo((int32_t)floor(microMap<float>(in, inMin, inMax, 0, count_-1)), count_);
+        value_ = microModulo((int32_t)floor(microMap<float>(in, inMin, inMax, 0, count_ - 1)), count_);
     }
 
     void mapInt(int32_t in, int32_t inMin, int32_t inMaxExclusive) override
     {
-        value_ = map(in, inMin, inMaxExclusive, 0, count_-1);
+        value_ = map(in, inMin, inMaxExclusive, 0, count_ - 1);
     }
 
     MicroParamEnum(const char **labels, int32_t value, int32_t count)
-        : MicroParam('e'), labels_(labels), count_(count)
+        : MicroParam(MicroParam::Type::ENUM), labels_(labels), count_(count)
     {
         value_ = microModulo(value, count_);
     }
 };
 
-// COMPLEX
+// ======================= Complex types =======================
 
 class MicroParamBlob : public MicroParam
 {
@@ -243,16 +268,17 @@ private:
 
 public:
     MicroParamBlob(uint8_t *buffer, uint32_t capacity, uint32_t length = 0)
-        : MicroParam('B'), data_(buffer), capacity_(capacity), length_(length) {}
+        : MicroParam(MicroParam::Type::BLOB), data_(buffer), capacity_(capacity), length_(length) {}
 
-    const uint8_t *data() const { return data_; }
-    uint8_t *data() { return data_; }
-    uint32_t length() const { return length_; }
-    uint32_t capacity() const { return capacity_; }
+    //const uint8_t *data() const { return data_; }
+    uint8_t *get() const { return data_; }
+    uint32_t getLength() const { return length_; }
+    uint32_t getCapacity() const { return capacity_; }
 
     bool set(const uint8_t *buffer, uint32_t length)
     {
-        if (length > capacity_) return false;
+        if (length > capacity_)
+            return false;
         memcpy(data_, buffer, length);
         length_ = length;
         return true;
@@ -272,25 +298,26 @@ private:
 
 public:
     MicroParamString(char *buffer, uint32_t capacity)
-        : MicroParam('s'), data_(buffer), capacity_(capacity), length_(0)
+        : MicroParam(MicroParam::Type::STRING), data_(buffer), capacity_(capacity), length_(0)
     {
-        if (capacity_ == 0 || data_ == nullptr) return;
+        if (capacity_ == 0 || data_ == nullptr)
+            return;
         length_ = strnlen(data_, capacity_ - 1);
         data_[length_] = '\0';
     }
 
-    const char *get() const { return data_; }
-    char *data() { return data_; }
-    uint32_t length() const { return length_; }
-    uint32_t capacity() const { return capacity_; }
+    //const char *get() const { return data_; }
+   char * get() const { return data_; }
+    uint32_t getLength() const { return length_; }
+    uint32_t getCapacity() const { return capacity_; }
 
     bool set(const char *s)
     {
-        if (!s || !data_ || capacity_ == 0) return false;
-        size_t new_len = strnlen(s, capacity_ - 1);
-        memcpy(data_, s, new_len);
-        data_[new_len] = '\0';
-        length_ = new_len;
+        size_t lengthNew = strlen(s);
+        if ( lengthNew >= capacity_-1 ) return false;
+        length_ = lengthNew;
+        memcpy(data_, s, length_);
+        data_[length_] = '\0';
         return true;
     }
 
@@ -308,8 +335,51 @@ public:
     }
 
     // Implicit conversion to const char*
-    operator const char*() const { return data_; }
+    operator const char *() const { return data_; }
 };
 
+// ======================= MicroParamBindKey =======================
+
+class MicroParamBindKey
+{
+private:
+    const char *key_;
+    const uint32_t keyHash_;
+    MicroParam &param_;
+
+public:
+    // --------- Constructors ---------
+    MicroParamBindKey(const char *k, MicroParam &param)
+        : key_(k), keyHash_(microHashFnv1a(k)), param_(param)
+    {
+    }
+
+    const char *getKey() const
+    {
+        return key_;
+    }
+
+    bool matches(const char *k) const
+    {
+        return strcmp(key_, k) == 0;
+    }
+
+    bool matches(uint32_t hash) const
+    {
+        return keyHash_ == hash;
+    }
+
+    bool matches(uint32_t hash, const char *addr) const
+    {
+        if (hash != keyHash_)
+            return false;
+        return strcmp(key_, addr) == 0;
+    }
+
+    MicroParam &  getParam()
+    {
+        return param_;
+    }
+};
 
 #endif // __MICRO_PARAM_H__
--- src/MicroParamOsc.h
+++ src/MicroParamOsc.h
@@ -1,114 +1,27 @@
 #include <MicroParam.h>
 #include <MicroOsc.h>
 
-// ======================= MicroParamBind =======================
-
-class MicroParamBind {
-  private:
-  enum Type : uint8_t { INT = 'i' , FLOAT = 'f', ENUM = 'e' } type_;
-  const char *key_;
-  const uint32_t keyHash_;
-  
-
-  union {
-    MicroParamInt   *i;
-    MicroParamFloat *f;
-    MicroParamEnum  *e;
-  } ptr_;
-
-  public:
-  // --------- Constructors ---------
-  MicroParamBind(const char *k, MicroParamInt &v)
-      : type_(INT), key_(k) , keyHash_(microHashFnv1a(k)) {
-    ptr_.i = &v;
-  }
-
-  MicroParamBind(const char *k, MicroParamFloat &v)
-      : type_(FLOAT), key_(k) , keyHash_(microHashFnv1a(k)) {
-    ptr_.f = &v;
-  }
-
-  MicroParamBind(const char *k, MicroParamEnum &v)
-      : type_(ENUM), key_(k) , keyHash_(microHashFnv1a(k)) {
-    ptr_.e = &v;
-  }
-
-  Type getType() const {
-    return type_;
-  }
-
-  const char *getKey() const {
-    return key_;
-  }
-
-  bool matches(const char *k) const {
-    return strcmp(key_, k) == 0;
-  }
-
-  bool matches(uint32_t hash) const {
-    return keyHash_ == hash;
-  }
-
-  bool matches(uint32_t hash, const char *addr) const {
-        if (hash != keyHash_) return false;
-        return strcmp(key_, addr) == 0; 
-    }
-
-  // --------- Access ---------
-  void setInt(int32_t v) {
-    switch (type_) {
-      case INT:   ptr_.i->set(v); break;
-      case FLOAT: ptr_.f->set((float)v); break;
-      case ENUM:  ptr_.e->set(v); break;
-    }
-  }
-
-  int32_t getInt() const {
-    switch (type_) {
-      case INT:   return ptr_.i->get();
-      case FLOAT: return (int32_t)ptr_.f->get();
-      case ENUM:  return ptr_.e->get();
-    }
-    return 0;
-  }
-
-  void setFloat(float v) {
-    switch (type_) {
-      case FLOAT: ptr_.f->set(v); break;
-      case INT:   ptr_.i->set((int32_t)floor(v)); break;
-      case ENUM:  ptr_.e->set((int32_t)floor(v)); break;
-    }
-  }
-
-  float getFloat() const {
-    switch (type_) {
-      case FLOAT: return ptr_.f->get();
-      case INT:   return (float)ptr_.i->get();
-      case ENUM:  return (float)ptr_.e->get();
-    }
-    return 0.0f;
-  }
-};
 
 
-bool microParamOscDispatch(MicroOscMessage &message, MicroParamBind *bindings, size_t bindingsCount)
+
+bool microParamOscDispatch(MicroOscMessage &message, MicroParamBindKey *bindings, size_t bindingsCount)
 {
     const char *addr = message.getOscAddress();
     const uint32_t addrHash = microHashFnv1a(addr);
 
-    for (size_t p = 0; p < bindingsCount; ++p)
+    for (size_t i = 0; i < bindingsCount; i++)
     {
-        MicroParamBind &binding = bindings[p];
+        MicroParamBindKey & binding = bindings[i];
 
         if (binding.matches(addrHash, addr)) // Could be optimized with hash comparison only
         {
                 switch (message.getTypeTag())
                 {
                 case 'i':
-                    binding.setInt(message.nextAsInt());
+                    binding.getParam().setInt(message.nextAsInt());
                     break;
                 case 'f':
-                    binding.setFloat(message.nextAsFloat());
+                    binding.getParam().setFloat(message.nextAsFloat());
                     break;
                 default:
                     // unsupported type
@@ -121,27 +34,28 @@ bool microParamOscDispatch(MicroOscMessage &message, MicroParamBind *bindings, s
     return false;
 };
 
-void microParamOscSend(MicroOsc &osc, MicroParamBind &binding)
+void microParamOscSend(MicroOsc &osc, MicroParamBindKey & binding)
 {
     const char *key = binding.getKey();
+    MicroParam & param = binding.getParam();
 
-    switch (binding.getType())
+    switch (param.getType())
     {
-    case 'e':
-    case 'i':
-        osc.sendInt(key, binding.getInt());
+    case MicroParam::Type::ENUM :
+    case MicroParam::Type::INT :
+        osc.sendInt(key, param.getInt());
         break;
-    case 'f':
-        osc.sendFloat(key, binding.getFloat());
+    case MicroParam::Type::FLOAT:
+        osc.sendFloat(key, param.getFloat());
         break;
     }
 };
 
-void microParamOscSend(MicroOsc &osc, MicroParamBind *bindings, size_t bindingsCount)
+void microParamOscSend(MicroOsc &osc, MicroParamBindKey *bindings, size_t bindingsCount)
 {
     for (size_t p = 0; p < bindingsCount; ++p)
     {
-        MicroParamBind &binding = bindings[p];
+        MicroParamBindKey & binding = bindings[p];
         microParamOscSend(osc, binding);
     };
 };
